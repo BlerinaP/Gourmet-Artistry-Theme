5cdd25672a6103102af4dac71ca86964cb2dddc4
Blerina <blerina1.p@gmail.com>
2019-12-21 15:21:58 +0100

    Adding page template
    Signed-off-by: Blerina <blerina1.p@gmail.com>

---
+++ page-recipes.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Template Name: Recipes
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Gourmet_Artistry
+ */
+
+get_header(); ?>
+
+    <div class="row column">
+    <div id="primary" class="content-area">
+        <main id="main" class="site-main" role="main">
+
+        </main><!-- #main -->
+    </div><!-- #primary -->
+
+<?php
+echo "</div>";
+get_footer();
